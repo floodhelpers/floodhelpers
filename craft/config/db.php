39949360b3262937e5c9bb297ec62a8e363aa3f4
Mike Stephens <mike@ayup.agency>
2015-12-29 09:36:23 +0000

    Live DB credentials

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,8 +7,6 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
-
 return array(
 
   '*' => array(
@@ -23,10 +21,10 @@ return array(
   ),
 
   'live' => array(
-    'server' => $url['host'],
-		'user' => $url['user'],
-		'password' => $url['pass'],
-		'database' => substr($url['path'], 1),
+    'server' => 'localhost',
+		'user' => 'fccb4cd72216',
+		'password' => '8dc1b2b7246953be',
+		'database' => 'floodhelpers',
   )
 
 );
